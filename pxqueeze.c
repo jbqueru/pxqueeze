46c7d3bc0a95fee739732b1de0c4869512c97231
Jean-Baptiste Queru <jbqueru@gmail.com>
2024-09-04 09:04:35 +0300

    Initial work on MTF (move-to-front) transform

---
+++ pxqueeze.c
@@ -0,0 +1,193 @@
+/*
+ * Copyright 2024 Jean-Baptiste M. "JBQ" "Djaybee" Queru
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+#include <stdio.h>
+#include <stdlib.h>
+
+unsigned char tga[192044];
+
+int pix_xy[320][200];
+
+int pix[64000];
+
+int pixmod[64000];
+
+int pixback[64000];
+
+unsigned char output[64000];
+
+unsigned char lzoutput[64000 * 6];
+
+int tgablue(int x, int y) {
+	return tga[18 + (x + y * 320) * 3];
+}
+
+int tgagreen(int x, int y) {
+	return tga[18 + (x + y * 320) * 3 + 1];
+}
+
+int tgared(int x, int y) {
+	return tga[18 + (x + y * 320) * 3 + 2];
+}
+
+void move_to_front() {
+	int maxpix = 0;
+	for (int i = 0; i < 64000; i++) {
+		if (pix[i] > maxpix) {
+			maxpix = pix[i];
+		}
+	}
+	printf("Max pixel value: %d\n", maxpix);
+
+	int* values = malloc((maxpix + 1) * sizeof(int));
+	for (int v = 0; v <= maxpix ; v++) {
+		values[v] = v;
+	}
+
+	for (int i = 0; i < 64000; i++) {
+		int c = 0;
+		for (int v = 0 ; v <= maxpix ; v++) {
+			if (pix[i] == values[v]) {
+				if (v > 0) {
+					values[0] = values[v];
+					values[v] = c;
+				}
+				pixmod[i] = v;
+				break;
+			}
+			int cc = values[v];
+			values[v] = c;
+			c = cc;
+		}
+	}
+}
+
+void back_from_front() {
+	int maxpix = 0;
+	for (int i = 0; i < 64000; i++) {
+		if (pixmod[i] > maxpix) {
+			maxpix = pixmod[i];
+		}
+	}
+	printf("Max offset value: %d\n", maxpix);
+
+	int* values = malloc((maxpix + 1) * sizeof(int));
+	for (int v = 0; v <= maxpix ; v++) {
+		values[v] = v;
+	}
+
+	for (int i = 0; i < 64000; i++) {
+		pixback[i] = values[pixmod[i]];
+		if (pixmod[i] > 0) {
+			int c = values[pixmod[i]];
+			for (int v = pixmod[i] - 1; v >= 0; v--) {
+				values[v + 1] = values[v];
+			}
+			values[0] = c;
+		}
+	}
+}
+
+void main() {
+	FILE* inputfile = fopen("out/gfx/jbq.tga", "rb");
+	fread(tga, 1, 192044, inputfile);
+	fclose(inputfile);
+
+	for (int y = 0; y < 200; y++) {
+		for (int x = 0; x < 320; x++) {
+			pix_xy[x][y] = (tgared(x, y) + tgagreen(x, y) + tgablue(x, y)) * 8 / 766;
+		}
+	}
+
+	for (int y = 0; y < 200; y++) {
+		for (int x = 0; x < 320; x++) {
+			pix[x + 320 * y] = pix_xy[x][y];
+		}
+	}
+
+	for (int n = 0; n < 64000; n++) {
+		output[n] = pix[n];
+	}
+
+	FILE* outputfile = fopen("out/gfx/pixels.bin", "wb");
+	fwrite(output, 1, 64000, outputfile);
+	fclose(outputfile);
+
+	move_to_front();
+
+	for (int n = 0; n < 64000; n++) {
+		output[n] = pixmod[n];
+	}
+
+	outputfile = fopen("out/gfx/mtf.bin", "wb");
+	fwrite(output, 1, 64000, outputfile);
+	fclose(outputfile);
+
+	back_from_front();
+
+	for (int n = 0; n < 64000; n++) {
+		output[n] = pixback[n];
+	}
+
+	outputfile = fopen("out/gfx/mtfback.bin", "wb");
+	fwrite(output, 1, 64000, outputfile);
+	fclose(outputfile);
+
+/*	int s = 0;
+
+	for (int target = 0; target < 64000; target++) {
+		int beststart = 0;
+		int bestlength = 0;
+		for (int start = 0; start < target; start++) {
+			int length;
+			for (length = 0; length < 64000 - target; length++) {
+				if (pix[start + length] != pix[target + length]) {
+					break;
+				}
+				if (length >= bestlength) {
+					bestlength = length;
+					beststart = start;
+				}
+			}
+		}
+		if (bestlength == 0) {
+//			printf("no match found for %d\n", target);
+		} else {
+			if (target + bestlength == 64000) {
+//				printf("input overflow\n");
+				bestlength--;
+			}
+//			printf("found match of %d pixels at %d for %d\n", bestlength, beststart, target);
+		}
+
+		lzoutput[s++] = bestlength >> 8;
+		lzoutput[s++] = bestlength % 256;
+		lzoutput[s++] = beststart >> 8;
+		lzoutput[s++] = beststart % 256;
+		lzoutput[s++] = pix[target + bestlength];
+		s++;
+
+		target += bestlength;
+	}
+
+	FILE* lzfile = fopen("out/gfx/lz.bin", "wb");
+	fwrite(lzoutput, 1, s, lzfile);
+	fclose(lzfile);
+*/
+}
